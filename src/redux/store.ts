af0509cbf1c7f7f1e5115929affcc62de71b14cc
Emiliano Ruiz Carletti <main@eruizc.dev>
2021-01-24 01:40:28 -0300

    Fix redux devtools

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -3,7 +3,12 @@ import availability from "./availability/reducer";
 import auth from "./auth/reducer";
 import orders from "./orders/reducer";
 
+function reduxDevtoolsExtension() {
+    const func = (window as any).__REDUX_DEVTOOLS_EXTENSION__;
+    return func ? func() : undefined
+}
+
 const reducers = combineReducers({ auth, availability, orders });
-export const store = createStore(reducers, (window as any)?.__REDUX_DEVTOOLS_EXTENSION__());
+export const store = createStore(reducers, reduxDevtoolsExtension());
 export default store;
 
